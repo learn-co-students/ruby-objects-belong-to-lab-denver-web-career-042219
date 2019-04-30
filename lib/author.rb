03762e7f26474bdf09c1e75926b8c2dfa998164e
drbarq <J.Tustin@gmail.com>
2019-04-30 17:24:08 -0600

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,12 @@
+class Author
+  attr_reader :name
+
+  def initialize
+  end
+
+  def name=(name)
+    @name = name
+  end 
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_reader
+
+
+  def initialize
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end 
+
+
+
+
+end
