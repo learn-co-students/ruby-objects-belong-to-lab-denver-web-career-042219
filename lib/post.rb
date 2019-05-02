b741c78a3a4af0db1dc4599f1da79311fb4e70f6
brookton <davidbrookton@gmail.com>
2019-05-02 13:43:01 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,24 @@
+class Post
+  attr_reader :title, :author
+
+
+  def initialize
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+end
