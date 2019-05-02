d1185f0d580c90066db0f9236c7e9c420a834b09
Christopher Tero <christopher.tero@gmail.com>
2019-05-01 19:59:27 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist
+  attr_accessor :name
+
+  def self.name
+    @name = name
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :title, :artist
+
+  def self.title
+    @title = title
+  end
+
+  def self.artist
+    @artist = artist
+  end
+
+end
